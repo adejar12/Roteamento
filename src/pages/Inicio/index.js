763efb16a1958046970e91cfbdd7250b37882990
adejar12 <adejarjunior18@gmail.com>
2022-01-24 21:29:52 -0300

    feat: adicionando páginas

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,10 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View,TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 export default function Inicio() {
+  const navigation = useNavigation();
+
   return (
     <View style={styles.container}>
       <Text>Página Inicio</Text>
+
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate("Publicacao");
+        }}
+      >
+        <Text> Ir para a publicação Interna</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate("PublicacaoExterna");
+        }}
+      >
+        <Text> Ir para a Publicação Externa</Text>
+      </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +33,8 @@ export default function Inicio() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
