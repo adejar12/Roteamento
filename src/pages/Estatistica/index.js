e89a29922bfd7895225e15324550840aaff5e523
adejar12 <adejarjunior18@gmail.com>
2022-01-26 08:24:03 -0300

    feat: adicionando rotas

--- src/pages/Estatistica/index.js
+++ src/pages/Estatistica/index.js
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 export default function Estatistica() {
+  const navigation = useNavigation();
+
   return (
     <View style={styles.container}>
       <Text>Estatistica</Text>
+
+      <TouchableOpacity
+        style={{
+          backgroundColor: "red",
+          width: "80%",
+          height: 30,
+          borderRadius: 10,
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: 80,
+        }}
+        onPress={() => {
+          navigation.navigate("EstatisticaComum");
+        }}
+      >
+        <Text
+          style={{
+            color: "white",
+          }}
+        >
+          {" "}
+          Ir para a estatistica 
+        </Text>
+      </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
+Estatistica.navigationOptions = ({ navigation }) => ({
+  headerLeft: () => (
+    <TouchableOpacity
+      style={{
+        paddingLeft: 12,
+      }}
+      onPress={() =>
+        navigation.goBack()
+      }
+    >
+      <Ionicons name="ios-chevron-back" size={32} color="red" />
+    </TouchableOpacity>
+  )
+});
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- src/pages/EstatisticaComum/index.js
+++ src/pages/EstatisticaComum/index.js
@@ -1,10 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,TouchableOpacity } from 'react-native';
+import { useNavigation } from "@react-navigation/native";
 
 export default function EstatisticaComum() {
+
+  const navigation = useNavigation();
+
   return (
     <View style={styles.container}>
       <Text>Estatistica Comum</Text>
+
+      <TouchableOpacity
+        style={{
+          backgroundColor: "red",
+          width: "80%",
+          height: 30,
+          borderRadius: 10,
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: 80,
+        }}
+        onPress={() => {
+          navigation.navigate("EstatistaPublicidade");
+        }}
+      >
+        <Text
+          style={{
+            color: "white",
+          }}
+        >
+          {" "}
+          Ir para a estatistica da publicidade
+        </Text>
+      </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
