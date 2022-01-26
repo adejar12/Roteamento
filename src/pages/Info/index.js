1508feff40a8732094b46835e9ca7e5103c4ec61
adejar12 <adejarjunior18@gmail.com>
2022-01-26 09:03:14 -0300

    refactor: removendo rotas não mais utilizadas

--- src/pages/Info/index.js
+++ src/pages/Info/index.js
@@ -1,20 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function Info({ navigation }) {
+export default function Info() {
   return (
     <View style={styles.container}>
-      <Text>Ir para a página sobre</Text>
-      <Button title="Sobre"
-        onPress={() => navigation.navigate('Sobre',{nome:'Adejar'})}
-      />
-
-      <Text>Ir para a  Página Home</Text>
-
-      <Button
-        title="Home"
-        onPress={() => navigation.navigate('Home')}
-      />
+      <Text>Página do perfil</Text>
     </View>
   );
 }
