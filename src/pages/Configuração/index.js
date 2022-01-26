7343c416f24ad34b0f2b97a989b9a3b1f389d78b
adejar12 <adejarjunior18@gmail.com>
2022-01-26 09:02:08 -0300

    feat: adicionando rotas

--- src/pages/Configuração/index.js
+++ src/pages/Configuração/index.js
@@ -1,10 +1,113 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,TouchableOpacity } from 'react-native';
+import { useNavigation } from "@react-navigation/native";
 
 export default function Configuracao() {
+
+  const navigation = useNavigation();
+
   return (
     <View style={styles.container}>
       <Text>Configurações</Text>
+
+      <TouchableOpacity
+        style={{
+          backgroundColor: "red",
+          width: "80%",
+          height: 30,
+          borderRadius: 10,
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: 60,
+        }}
+        onPress={() => {
+          navigation.navigate("Info");
+        }}
+      >
+        <Text
+          style={{
+            color: "white",
+          }}
+        >
+          {" "}
+          Ir para a o perfil 
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={{
+          backgroundColor: "red",
+          width: "80%",
+          height: 30,
+          borderRadius: 10,
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: 60,
+        }}
+        onPress={() => {
+          navigation.navigate("Sobre",{
+            nome: 'Adejar',
+          });
+        }}
+      >
+        <Text
+          style={{
+            color: "white",
+          }}
+        >
+          {" "}
+          Ir para o sobre
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={{
+          backgroundColor: "red",
+          width: "80%",
+          height: 30,
+          borderRadius: 10,
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: 60,
+        }}
+        onPress={() => {
+          navigation.navigate("Publicacao");
+        }}
+      >
+        <Text
+          style={{
+            color: "white",
+          }}
+        >
+          {" "}
+          Ir para uma publicações
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={{
+          backgroundColor: "red",
+          width: "80%",
+          height: 30,
+          borderRadius: 10,
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: 60,
+        }}
+        onPress={() => {
+          navigation.navigate("PublicacaoConfig");
+        }}
+      >
+        <Text
+          style={{
+            color: "white",
+          }}
+        >
+          {" "}
+          Ir para uma publicação nas configurações
+        </Text>
+      </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
